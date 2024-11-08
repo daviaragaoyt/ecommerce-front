21c4dfb2c00ffcc7d958603381690150bd22c8f0
Davi Aragao <daviaragaodf1@gmail.com>
2024-11-08 18:42:08 -0300

    nova versao

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Certifique-se que esta é a URL da sua API
+  baseURL: 'https://ecommerce-py.vercel.app', // Certifique-se que esta é a URL da sua API
 });
 
 export default api;
